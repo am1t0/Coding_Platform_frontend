5f932e0aa8294f2240e1b50d813eaf6ec874f0ea
am1t0 <amiitt43@gmail.com>
2024-09-24 15:29:12 +0530

    api changed

--- js/codeforces.js
+++ js/codeforces.js
@@ -3,6 +3,47 @@ let ind = 0;
 
 let content = document.getElementById('details1')
 
+// Fetch the contest data from Clist API
+let all_contest = fetch('https://clist.by/api/v2/contest?username=amey___25&api_key=b78c3701bceae65245c313c63104dfbdd6c092f4');
+
+// Process the response
+all_contest.then((response) => {
+  return response.json(); // Parse the response as JSON
+})
+.then((data) => {
+  // Platforms to filter
+  const platforms = {
+    codeforces: [],
+    leetcode: [],
+    codechef: [],
+    hackerrank: []
+  };
+
+  // Filter contests and assign to respective platform arrays
+  data.objects.forEach(contest => {
+    const host = contest.host || contest.resource;
+
+    if (host.includes('codeforces.com')) {
+      platforms.codeforces.push(contest);
+    } else if (host.includes('leetcode.com')) {
+      platforms.leetcode.push(contest);
+    } else if (host.includes('codechef.com')) {
+      platforms.codechef.push(contest);
+    } else if (host.includes('hackerrank.com')) {
+      platforms.hackerrank.push(contest);
+    }
+  });
+
+  // Log the contests for each platform
+  console.log('Codeforces Contests:', platforms.codeforces);
+  console.log('LeetCode Contests:', platforms.leetcode);
+  console.log('CodeChef Contests:', platforms.codechef);
+  console.log('HackerRank Contests:', platforms.hackerrank);
+})
+.catch((error) => {
+  console.error('Error:', error);
+});
+
 
 let codeforces = fetch('https://kontests.net/api/v1/codeforces');
 
